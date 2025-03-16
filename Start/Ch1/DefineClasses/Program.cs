38edfda3a82ff4574fcf4fe33d4a9d94763db8f3
Mithila Damruwan <mithiladamruwan21@gmail.com>
2025-03-16 12:19:04 +0530

    Edited

--- Start/Ch1/DefineClasses/Program.cs
+++ Start/Ch1/DefineClasses/Program.cs
@@ -2,6 +2,9 @@
 // Defining and instantiating classes
 
 // TODO: Create some new Rectangle objects with dimensions
+Rectangle rect1 = new Rectangle(10, 20);
+Rectangle rect2 = new Rectangle(30);
 
-
+console.writeline(rect1.GetArea());
+console.writeline(rect2.GetArea());
 // TODO: Change the values of width and height
